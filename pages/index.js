10079a0fed388dac31b49e074988c074b29c5abd
faisalgit1 <mdfaisalahmed520@gmail.com>
2023-01-12 17:39:40 +0600

    how-to-record-section-done

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Features from "../components/Features";
+import HowToRecord from "../components/HowToRecord";
 import Main from "../components/Layout/Main";
 
 export default function Home() {
     return (
         <Main title="Plugged In | Record Everything Online" className="">
+            <HowToRecord></HowToRecord>
             <Features></Features>
-
         </Main>
     );
 }
