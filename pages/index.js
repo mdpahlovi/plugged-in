91ef006dc585f3d8bac1e82b73a96a26c7073b5d
gobindacb <p.gobinda.cb@gmail.com>
2023-01-12 18:11:51 +0600

    faq and review added by gobinda

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Features from "../components/Features";
 import Main from "../components/Layout/Main";
+import RevisewSlider from "../components/Review/ReviewSlider";
+import FaqData from "../components/Faq/FaqData"
 import ScreenRecorder from "../components/ScreenRecorder";
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
     <Main title="Plugged In | Record Everything Online" className="w-full">
       <ScreenRecorder></ScreenRecorder>
       <Features></Features>
+      <RevisewSlider></RevisewSlider>
+      <FaqData></FaqData>
     </Main>
   );
 }
