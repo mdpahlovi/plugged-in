64bde8558aaa61dbced7de3c6590452ec133fad3
Fahim Faisal <mdfahimfaisal000@gmail.com>
2023-01-12 15:24:11 +0600

    recorder design

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Features from "../components/Features";
 import Main from "../components/Layout/Main";
+import ScreenRecorder from "../components/ScreenRecorder";
 
 export default function Home() {
-    return (
-        <Main title="Plugged In | Record Everything Online" className="">
-            <Features></Features>
-
-        </Main>
-    );
+  return (
+    <Main title="Plugged In | Record Everything Online" className="w-full">
+      <ScreenRecorder></ScreenRecorder>
+      <Features></Features>
+    </Main>
+  );
 }
