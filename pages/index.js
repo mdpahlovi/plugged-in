c6299b229c4b9bed546bc3a359605011ff4ceb29
Electra51 <safayetnurelectra@gmail.com>
2023-01-12 18:00:29 +0600

    herosection added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Features from "../components/Features";
+import Hero from "../components/Layout/Hero";
 import Main from "../components/Layout/Main";
 
 export default function Home() {
     return (
         <Main title="Plugged In | Record Everything Online" className="">
+            <Hero></Hero>
             <Features></Features>
 
         </Main>
