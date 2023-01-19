ba913238dc5a341cd458591f34b39f3ab485049e
faisalgit1 <mdfaisalahmed520@gmail.com>
2023-01-19 21:04:37 +0600

    registration-validation-added

--- pages/register.js
+++ pages/register.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 const Registration = () => {
     // Registration using react-hook-form
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors }, watch } = useForm();
 
     const handleRegistration = (data) => {
         axios
@@ -27,25 +27,49 @@ const Registration = () => {
                         <label htmlFor="name" className="block font-semibold">
                             Full Name
                         </label>
-                        <input type="text" {...register("name")} id="name" placeholder="Full Name" className="input" />
+                        <input type="text" {...register("name", { required: true })} id="name" placeholder="Full Name" className={errors.name ? "input border-red-600" : "input"} />
+                        <error className="text-red-600">
+                            {errors.name?.type === "required" && "Name is required"}
+                        </error>
                     </div>
                     <div className="space-y-1">
                         <label htmlFor="email" className="block font-semibold">
                             Email
                         </label>
-                        <input type="email" {...register("email")} id="email" placeholder="Your Email" className="input" />
+                        <input type="email" {...register("email", { required: true })} id="email" placeholder="Your Email" className={errors.email ? "input border-red-600" : "input"} />
+                        <error className="text-red-600">
+                            {errors.email?.type === "required" && "Email is required"}
+                        </error>
                     </div>
                     <div className="space-y-1">
                         <label htmlFor="password" className="block font-semibold">
                             Password
                         </label>
-                        <input type="password" {...register("password")} id="password" placeholder="Your Password" className="input" />
+                        <input type="password" {...register("password", {
+                            required: true, minLength: 6, maxLength: 10,
+                        })} id="password" placeholder="Your Password" className={errors.password ? "input border-red-600" : "input"} />
+                        <error className="text-red-600">
+                            {errors?.password?.type === "required" && "Password is required"}
+                            {errors?.password?.type === "minLength" &&
+                                "Entered password is less than 6 characters"}
+                            {errors?.password?.type === "maxLength" &&
+                                "Entered password is more than 10 characters"}
+                        </error>
                     </div>
                     <div className="space-y-1">
                         <label htmlFor="password" className="block font-semibold">
                             Confirm Password
                         </label>
-                        <input type="password" name="cPassword" id="cPassword" placeholder="Confirm Password" className="input" />
+                        <input type="password" {...register("cPassword", {
+                            validate: data => {
+                                if (watch("password") !== data) {
+                                    return "Password Not Matched"
+                                }
+                            }
+                        })} id="cPassword" placeholder="Confirm Password" className={errors.cPassword ? "input border-red-600" : "input"} />
+                        <error className="text-red-600">
+                            {errors?.cPassword?.message}
+                        </error>
                     </div>
                     <div className="pt-2">
                         <Button className="w-full" type="submit">
