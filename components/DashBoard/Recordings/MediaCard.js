2a4f34e5a9792f077472ee6c398da6a4223132f5
Fahim Faisal <mdfahimfaisal000@gmail.com>
2023-01-24 02:03:22 +0600

    delete and update

--- components/DashBoard/Recordings/MediaCard.js
+++ components/DashBoard/Recordings/MediaCard.js
@@ -5,72 +5,132 @@ import { format, parseISO } from "date-fns";
 import { Button, IconButton } from "../../Buttons";
 import audioImage from "../../../public/logo/audioImage.png";
 import { MdEditNote, MdDeleteSweep } from "react-icons/md";
+import { toast } from "react-toastify";
 
-const MediaCard = ({ media }) => {
-    const { _id, date, mediaType, mediaUrl } = media;
-    const [isEditing, setIsEditing] = useState(false);
+const MediaCard = ({ media, refetch, setDeletingRecord }) => {
+  const { _id, date, mediaType, mediaUrl, details, title } = media;
+  const [isEditing, setIsEditing] = useState(false);
 
-    const date_is = format(parseISO(date), "PP");
-    const time_is = format(parseISO(date), "p");
+  const date_is = format(parseISO(date), "PP");
+  const time_is = format(parseISO(date), "p");
 
-    const handelEdit = (id) => {
-        setIsEditing(true);
-        console.log(id);
+  const handelEdit = (data) => {
+    setIsEditing(!isEditing);
+  };
+
+  const handelDelete = (id) => {
+    setDeletingRecord(media);
+
+    console.log(id);
+  };
+
+  const { register, handleSubmit } = useForm();
+  const handleEdit = (data) => {
+    const updatingRecord = {
+      _id,
+      title: data.title,
+      details: data.details,
     };
 
-    const handelDelete = (id) => {
-        console.log(id);
-    };
-
-    const { register, handleSubmit } = useForm();
-    const handleDetails = (data) => {
-        setIsEditing(false);
+    fetch(`https://plugged-in-server.vercel.app/record`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatingRecord),
+    })
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
-    };
+        if (data.matchedCount > 0) {
+          refetch();
+          toast.success("Record updated successfully");
+          setIsEditing(false);
+        }
+      });
+    console.log(updatingRecord);
+  };
 
-    return (
-        <div className="">
-            <figure>
-                {mediaType === "video" || mediaType === "screen" ? (
-                    <video src={mediaUrl} controls className="aspect-video w-full rounded-lg"></video>
-                ) : (
-                    <Image src={audioImage} alt="" className="aspect-video w-full rounded-lg" />
-                )}
-            </figure>
-            <div className="-mt-4 border border-t-0 rounded-lg p-5">
-                <div className="pt-2 mb-1 flex justify-between items-center">
-                    <p className="text-sm">
-                        {date_is} at {time_is}
-                    </p>
-                    <div className="space-x-4">
-                        <IconButton onClick={() => handelEdit(_id)}>
-                            <MdEditNote className="text-lg" />
-                        </IconButton>
-                        <IconButton onClick={() => handelDelete(_id)}>
-                            <MdDeleteSweep className="text-lg" />
-                        </IconButton>
-                    </div>
-                </div>
-                {isEditing ? (
-                    <form onSubmit={handleSubmit(handleDetails)} className="space-y-1">
-                        <textarea {...register("title")} className="textarea textarea-bordered w-full" rows="1" placeholder="Recording Title"></textarea>
-                        <textarea {...register("details")} className="textarea textarea-bordered w-full" rows="3" placeholder="Recording Details"></textarea>
-                        <Button type="submit" className="w-full">
-                            Submit
-                        </Button>
-                    </form>
-                ) : (
-                    <>
-                        <h3 className="text-xl leading-6 font-semibold">If a dog chews shoes whose shoes does he choose?</h3>
-                        <p className="mt-1.5 leading-5">
-                            Photos contain data that can give you useful information about the picture. Information such as shutter speed and focal length are
-                            stored inside an image. Likewise, you can find out where the photo was taken by looking at the location information
-                        </p>
-                    </>
-                )}
-            </div>
+  return (
+    <div className="">
+      <figure>
+        {mediaType === "video" || mediaType === "screen" ? (
+          <video
+            src={mediaUrl}
+            controls
+            className="aspect-video w-full rounded-lg"
+          ></video>
+        ) : (
+          <Image
+            src={audioImage}
+            alt=""
+            className="aspect-video w-full rounded-lg"
+          />
+        )}
+      </figure>
+      <div className="-mt-4 border border-t-0 rounded-lg p-5">
+        <div className="pt-2 mb-1 flex justify-between items-center">
+          <p className="text-sm">
+            {date_is} at {time_is}
+          </p>
+          <div className="space-x-4">
+            <IconButton onClick={() => handelEdit(_id)}>
+              <MdEditNote className="text-lg" />
+            </IconButton>
+            <label htmlFor="deleteModal">
+              <IconButton onClick={() => handelDelete(_id)}>
+                <MdDeleteSweep className="text-lg" />
+              </IconButton>
+            </label>
+          </div>
         </div>
-    );
+        {isEditing ? (
+          <form onSubmit={handleSubmit(handleEdit)} className="space-y-1">
+            <textarea
+              {...register("title")}
+              className="textarea textarea-bordered w-full"
+              rows="1"
+              defaultValue={title}
+              placeholder="Recording Title"
+            ></textarea>
+            <textarea
+              {...register("details")}
+              className="textarea textarea-bordered w-full"
+              rows="3"
+              placeholder="Recording Details"
+              defaultValue={details}
+            ></textarea>
+            <Button type="submit" className="w-full">
+              Submit
+            </Button>
+          </form>
+        ) : (
+          <>
+            {title ? (
+              <h3 className="text-xl leading-6 font-semibold">{title}</h3>
+            ) : (
+              <textarea
+                // {...register("title")}
+                className="textarea textarea-bordered w-full"
+                rows="1"
+                placeholder="Recording Title"
+              ></textarea>
+            )}
+            {details ? (
+              <p className="mt-1.5 leading-5">{details}</p>
+            ) : (
+              <textarea
+                // {...register("details")}
+                className="textarea textarea-bordered w-full"
+                rows="3"
+                placeholder="Recording Details"
+              ></textarea>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default MediaCard;
--- hooks/useSetMediaToDb.js
+++ hooks/useSetMediaToDb.js
@@ -2,23 +2,23 @@ import React, { useEffect, useState } from "react";
 import { useAuth } from "./useAuth";
 
 const useSetMediaToDb = (media, user) => {
-    const [confirmation, setConfirmation] = useState("");
+  const [confirmation, setConfirmation] = useState("");
 
-    useEffect(() => {
-        if (user && media) {
-            fetch("https://plugged-in-server.vercel.app/userRecords", {
-                method: "POST",
-                headers: {
-                    "content-type": "application/json",
-                },
-                body: JSON.stringify(media),
-            })
-                .then((res) => res.json())
-                .then((data) => setConfirmation(data));
-        }
-    }, [user, media]);
+  useEffect(() => {
+    if (user && media) {
+      fetch("https://plugged-in-server.vercel.app/userRecords", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(media),
+      })
+        .then((res) => res.json())
+        .then((data) => setConfirmation(data));
+    }
+  }, [user, media]);
 
-    return { confirmation };
+  return { confirmation };
 };
 
 export default useSetMediaToDb;
