5175b8be256e6ec5de960e7b0a3d0584e3d6fc98
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-01-24 17:44:47 +0600

    Polish Deleting Modal

--- components/DashBoard/Recordings/MediaCard.js
+++ components/DashBoard/Recordings/MediaCard.js
@@ -1,59 +1,38 @@
+import axios from "axios";
 import Image from "next/image";
 import { useState } from "react";
+import { toast } from "react-toastify";
 import { useForm } from "react-hook-form";
 import { format, parseISO } from "date-fns";
 import { Button, IconButton } from "../../Buttons";
 import audioImage from "../../../public/logo/audioImage.png";
 import { MdEditNote, MdDeleteSweep } from "react-icons/md";
-import { toast } from "react-toastify";
 
-const MediaCard = ({ media, refetch, setDeletingRecord }) => {
-  const { _id, date, mediaType, mediaUrl, details, title } = media;
-  const [isEditing, setIsEditing] = useState(false);
+const MediaCard = ({ media, refetch, setDeletingRecordId }) => {
+    const { _id, date, mediaType, mediaUrl, details, title } = media;
+    const [isEditing, setIsEditing] = useState(false);
 
-  const date_is = format(parseISO(date), "PP");
-  const time_is = format(parseISO(date), "p");
+    const date_is = format(parseISO(date), "PP");
+    const time_is = format(parseISO(date), "p");
 
-  const handelEdit = (data) => {
-    setIsEditing(!isEditing);
-  };
-
-  const handelDelete = (id) => {
-    setDeletingRecord(media);
-
-    console.log(id);
-  };
-
-  const { register, handleSubmit } = useForm();
-  const handleEdit = (data) => {
-    const updatingRecord = {
-      _id,
-      title: data.title,
-      details: data.details,
+    const { register, handleSubmit } = useForm();
+    const handleEdit = ({ title, details }) => {
+        const updatingRecord = { _id, title, details };
+        axios
+            .put(`https://plugged-in-server.vercel.app/record`, updatingRecord)
+            .then((res) => {
+                if (res.data.matchedCount > 0) {
+                    refetch();
+                    toast.success("Record updated successfully");
+                    setIsEditing(false);
+                }
+            })
+            .catch((error) => console.log(error.message));
     };
-
-    fetch(`https://plugged-in-server.vercel.app/record`, {
-      method: "PUT",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(updatingRecord),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.matchedCount > 0) {
-          refetch();
-          toast.success("Record updated successfully");
-          setIsEditing(false);
-        }
-      });
-    console.log(updatingRecord);
-  };
 
     return (
         <div className="">
-            <figure>
+            <div>
                 {mediaType === "video" || mediaType === "screen" ? (
                     <video src={mediaUrl} controls className="video"></video>
                 ) : (
@@ -62,87 +41,50 @@ const MediaCard = ({ media, refetch, setDeletingRecord }) => {
                         <Image src={audioImage} alt="" className="w-1/2 mx-auto rounded-lg" />
                     </div>
                 )}
-            </figure>
+            </div>
             <div className="-mt-4 border border-t-0 rounded-lg p-5">
                 <div className="pt-2 flex justify-between items-center">
                     <p className="text-sm">
                         {date_is} at {time_is}
                     </p>
                     <div className="space-x-4">
-                        <IconButton onClick={() => handelEdit(_id)}>
+                        <IconButton onClick={() => setIsEditing(!isEditing)}>
                             <MdEditNote className="text-lg" />
                         </IconButton>
-                        <IconButton onClick={() => handelDelete(_id)}>
+                        <IconButton onClick={() => setDeletingRecordId(_id)}>
                             <MdDeleteSweep className="text-lg" />
                         </IconButton>
                     </div>
                 </div>
-                {isEditing ? (
-                    <form onSubmit={handleSubmit(handleDetails)} className="pt-1.5 space-y-1">
-                        <textarea {...register("title")} className="textarea textarea-bordered w-full" rows="1" placeholder="Recording Title"></textarea>
-                        <textarea {...register("details")} className="textarea textarea-bordered w-full" rows="3" placeholder="Recording Details"></textarea>
+                {isEditing || !title || !details ? (
+                    <form onSubmit={handleSubmit(handleEdit)} className="mt-1.5 space-y-1">
+                        <textarea
+                            {...register("title")}
+                            className="textarea textarea-bordered w-full"
+                            rows="1"
+                            placeholder="Recording Title"
+                            defaultValue={title}
+                        ></textarea>
+                        <textarea
+                            {...register("details")}
+                            className="textarea textarea-bordered w-full"
+                            rows="3"
+                            placeholder="Recording Details"
+                            defaultValue={details}
+                        ></textarea>
                         <Button type="submit" className="w-full">
                             Submit
                         </Button>
                     </form>
                 ) : (
-                    <div className="space-y-1.5">
-                        <h3 className="text-xl leading-6 font-semibold">If a dog chews shoes whose shoes does he choose?</h3>
-                        <p className="leading-5">
-                            Photos contain data that can give you useful information about the picture. Information such as shutter speed and focal length are
-                            stored inside an image. Likewise, you can find out where the photo was taken by looking at the location information
-                        </p>
-                    </div>
+                    <>
+                        <h3 className="text-xl leading-6 font-semibold">{title}</h3>
+                        <p className="mt-1.5 leading-5">{details}</p>
+                    </>
                 )}
             </div>
         </div>
-        {isEditing ? (
-          <form onSubmit={handleSubmit(handleEdit)} className="space-y-1">
-            <textarea
-              {...register("title")}
-              className="textarea textarea-bordered w-full"
-              rows="1"
-              defaultValue={title}
-              placeholder="Recording Title"
-            ></textarea>
-            <textarea
-              {...register("details")}
-              className="textarea textarea-bordered w-full"
-              rows="3"
-              placeholder="Recording Details"
-              defaultValue={details}
-            ></textarea>
-            <Button type="submit" className="w-full">
-              Submit
-            </Button>
-          </form>
-        ) : (
-          <>
-            {title ? (
-              <h3 className="text-xl leading-6 font-semibold">{title}</h3>
-            ) : (
-              <textarea
-                // {...register("title")}
-                className="textarea textarea-bordered w-full"
-                rows="1"
-                placeholder="Recording Title"
-              ></textarea>
-            )}
-            {details ? (
-              <p className="mt-1.5 leading-5">{details}</p>
-            ) : (
-              <textarea
-                // {...register("details")}
-                className="textarea textarea-bordered w-full"
-                rows="3"
-                placeholder="Recording Details"
-              ></textarea>
-            )}
-          </>
-        )}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MediaCard;
--- pages/dashboard/recordings.js
+++ pages/dashboard/recordings.js
@@ -1,47 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
-import dynamic from "next/dynamic";
 import { useState } from "react";
-import DeleteCnfirmModal from "../../components/DashBoard/Recordings/DeleteCnfirmModal";
+import DeleteConfirmModal from "../../components/DashBoard/Recordings/DeleteConfirmModal";
 import MediaCard from "../../components/DashBoard/Recordings/MediaCard";
 import Dashboard from "../../components/Layout/Dashboard";
 import { useAuth } from "../../hooks/useAuth";
 
 const Recordings = () => {
-  const { authUser } = useAuth();
+    const { authUser } = useAuth();
+    const [deletingRecordId, setDeletingRecordId] = useState(null);
 
-  const [deletingRecord, setDeletingRecord] = useState(null);
+    const { data: medias = [], refetch } = useQuery({
+        queryKey: ["userMedia", authUser],
+        queryFn: () => fetch(`https://plugged-in-server.vercel.app/userMedia?email=${authUser?.email}`).then((res) => res.json()),
+    });
 
-  const { data: medias = [], refetch } = useQuery({
-    queryKey: ["userMedia", authUser],
-    queryFn: () =>
-      fetch(
-        `https://plugged-in-server.vercel.app/userMedia?email=${authUser?.email}`
-      ).then((res) => res.json()),
-  });
-
-  // grid-template-columns: repeat(auto-fill, minmax(96px, 1fr))
-
-  return (
-    <Dashboard title={`${authUser?.displayName} Recordings in PluggedIn`}>
-      <div className="grid grid-cols-[repeat(auto-fill,_minmax(20rem,_1fr))] gap-6">
-        {medias.map((media) => (
-          <MediaCard
-            key={media._id}
-            media={media}
-            refetch={refetch}
-            setDeletingRecord={setDeletingRecord}
-          ></MediaCard>
-        ))}
-        {deletingRecord && (
-          <DeleteCnfirmModal
-            deletingRecord={deletingRecord}
-            setDeletingRecord={setDeletingRecord}
-            refetch={refetch}
-          />
-        )}
-      </div>
-    </Dashboard>
-  );
+    return (
+        <Dashboard title={`${authUser?.displayName} Recordings in PluggedIn`}>
+            <div className="grid grid-cols-[repeat(auto-fill,_minmax(20rem,_1fr))] gap-6">
+                {medias.map((media) => (
+                    <MediaCard key={media._id} media={media} refetch={refetch} setDeletingRecordId={setDeletingRecordId}></MediaCard>
+                ))}
+                <DeleteConfirmModal deletingRecordId={deletingRecordId} setDeletingRecordId={setDeletingRecordId} refetch={refetch} />
+            </div>
+        </Dashboard>
+    );
 };
 
 export default Recordings;
