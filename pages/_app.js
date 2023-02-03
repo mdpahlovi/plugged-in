a69273709d740f57346513ad1be5296a2b93a5dd
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-02-04 00:03:13 +0600

    .vscode Setting Added

--- pages/_app.js
+++ pages/_app.js
@@ -2,26 +2,22 @@ import { ThemeProvider } from "../hooks/useTheme";
 import "../styles/globals.css";
 import { UserContext } from "../hooks/useAuth";
 import Toastify from "../components/Toastify";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
-import SocketProvider from "../Contexts/SocketProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SocketProvider from "../contexts/SocketProvider";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
-  return (
-    <SocketProvider>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <UserContext>
-            <Component {...pageProps} />
-            <Toastify />
-          </UserContext>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </SocketProvider>
-  );
+    return (
+        <SocketProvider>
+            <QueryClientProvider client={queryClient}>
+                <ThemeProvider>
+                    <UserContext>
+                        <Component {...pageProps} />
+                        <Toastify />
+                    </UserContext>
+                </ThemeProvider>
+            </QueryClientProvider>
+        </SocketProvider>
+    );
 }
