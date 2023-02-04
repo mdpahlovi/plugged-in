6752a0fcf4e570df37c55f374ab20a35a7c60729
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-02-04 23:13:18 +0600

    Message Dynamic Route Added

---
+++ components/DashBoard/Message/MessageUser.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+const MessageUser = ({ user, active }) => {
+    const { name, avatar } = user;
+    return (
+        <div
+            className={`flex items-center px-4 py-2 transition duration-300 border-b cursor-pointer ${
+                active ? "bg-base-content/10" : "hover:bg-base-content/10"
+            }`}
+        >
+            <Image className="mask mask-squircle" src={avatar} alt="username" width={40} height={40} />
+            <div className="w-full">
+                <div className="-mb-1 flex justify-between items-center">
+                    <h3 className="ml-2 font-semibold">{name}</h3>
+                    <p className="ml-2 text-sm">25 minutes</p>
+                </div>
+                <p className="ml-2 text-sm">bye</p>
+            </div>
+        </div>
+    );
+};
+
+export default MessageUser;
---
+++ pages/dashboard/message/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Message from "../../../components/Layout/Message";
+
+const index = () => {
+    return (
+        <Message>
+            <h1 className="h-full flex justify-center items-center">Chat With Your Team</h1>
+        </Message>
+    );
+};
+
+export default index;
