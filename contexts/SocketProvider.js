85bb3bdb9b008f87630f58c8b94e2411d1f54085
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-02-04 23:29:43 +0600

    Content folder error fixed

---
+++ contexts/SocketProvider.js
@@ -0,0 +1,15 @@
+import React, { createContext } from "react";
+import { io } from "socket.io-client";
+
+const socket = io.connect("http://localhost:5000/");
+export const SocketContext = createContext();
+
+const SocketProvider = ({ children }) => {
+    const socketInfo = {
+        socket,
+    };
+
+    return <SocketContext.Provider value={socketInfo}>{children}</SocketContext.Provider>;
+};
+
+export default SocketProvider;
