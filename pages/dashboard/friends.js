d73354ef15a30d793c762893480540e4a7d90510
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-02-05 23:47:07 +0600

    Friends Error Fixed

--- pages/dashboard/friends.js
+++ pages/dashboard/friends.js
@@ -5,38 +5,26 @@ import FriendCard from "../../components/DashBoard/Friends/FriendCard";
 import Dashboard from "../../components/Layout/Dashboard";
 import { useAuth } from "../../hooks/useAuth";
 
-const friends = () => {
-  const { authUser } = useAuth();
-  const [disconnectingFriend, setDisconnectingFriend] = useState(null);
+const Friends = () => {
+    const { authUser } = useAuth();
+    const [disconnectingFriend, setDisconnectingFriend] = useState(null);
 
-  const { data: friends = [], refetch: friendListRefetch } = useQuery({
-    queryKey: ["friends", authUser],
-    queryFn: () =>
-      fetch(`http://localhost:5000/friends?email=${authUser?.email}`).then(
-        (res) => res.json()
-      ),
-  });
+    const { data: friends = [], refetch: friendListRefetch } = useQuery({
+        queryKey: ["friends", authUser],
+        queryFn: () => fetch(`http://localhost:5000/friends?email=${authUser?.email}`).then((res) => res.json()),
+    });
 
-  return (
-    <Dashboard>
-      <p>Friends</p>
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-5">
-        {friends?.map((friend) => (
-          <FriendCard
-            key={friend?.email}
-            friend={friend}
-            setDisconnectingFriend={setDisconnectingFriend}
-          />
-        ))}
-      </div>
-      {disconnectingFriend && (
-        <DisconnectModal
-          disconnectingFriend={disconnectingFriend}
-          friendListRefetch={friendListRefetch}
-        />
-      )}
-    </Dashboard>
-  );
+    return (
+        <Dashboard>
+            <p>Friends</p>
+            <div className="grid grid-cols-1 lg:grid-cols-4 gap-5">
+                {friends?.map((friend) => (
+                    <FriendCard key={friend?.email} friend={friend} setDisconnectingFriend={setDisconnectingFriend} />
+                ))}
+            </div>
+            {disconnectingFriend && <DisconnectModal disconnectingFriend={disconnectingFriend} friendListRefetch={friendListRefetch} />}
+        </Dashboard>
+    );
 };
 
-export default friends;
+export default Friends;
--- pages/dashboard/message/index.js
+++ pages/dashboard/message/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Message from "../../../components/Layout/Message";
 
-const index = () => {
+const ChatIndex = () => {
     return (
         <Message>
             <h1 className="h-full flex justify-center items-center">Chat With Your Team</h1>
@@ -9,4 +9,4 @@ const index = () => {
     );
 };
 
-export default index;
+export default ChatIndex;
