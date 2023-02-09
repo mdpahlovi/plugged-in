48b2a8f86bed3aabe22cdba98ae3d97366ba8da9
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-02-09 22:20:43 +0600

    Member add and remove functionaly added

--- pages/dashboard/add_member.js
+++ pages/dashboard/add_member.js
@@ -4,36 +4,74 @@ import { jwt_axios } from "../../utilities/api";
 import { useQuery } from "@tanstack/react-query";
 import { useAuth } from "../../hooks/useAuth";
 import Member from "../../components/DashBoard/Add Member/Member";
+import useGetUser from "../../hooks/useGetUser";
+import { toast } from "react-toastify";
+import MemberCardLoader from "../../components/Common/MemberCardLoader";
 
 const AddMember = () => {
     const { authUser } = useAuth();
+    const { userLoading, user, userRefetch } = useGetUser(authUser?.email);
 
     const {
         data: team,
         isLoading: teamLoading,
         refetch: teamRefetch,
     } = useQuery({
-        queryKey: ["team", authUser],
-        queryFn: () => jwt_axios(`/team/${authUser?.email}`).then((res) => res.data),
+        queryKey: ["team", user],
+        queryFn: () => jwt_axios(`/team/${user?.team[0].leader}`).then((res) => res.data),
     });
 
     const handleAddMember = (member) => {
-        jwt_axios.put(`/team/${authUser?.email}`, { members: [...team?.members, member] }).then((res) => {
-            teamRefetch();
-            console.log(res.data);
+        jwt_axios.patch(`/team/${user?.team[0].leader}`, { members: [...team?.members, member] }).then((res) => {
+            if (res.data.acknowledged) {
+                jwt_axios
+                    .patch(`/user/${member?.email}`, { team: [...(member?.team ? member.team : []), { name: team?.name, leader: team?.leader }] })
+                    .then((res) => {
+                        teamRefetch();
+                        toast.success("Team Register to PluggedIn");
+                    })
+                    .catch((error) => console.log(error));
+            }
         });
     };
 
-    return (
-        <DashBoard title="Add Member">
-            {team?.leader_mail === authUser?.email ? <AddTeamMember handleAddMember={handleAddMember} /> : ""}
-            <div className="mt-6">
-                {team?.members?.map((member, index) => (
-                    <Member key={member?._id} member={member} />
+    const handleDeleteCard = (email) => {
+        const restMembers = team?.members?.filter((member) => member?.email !== email);
+        const restTeam = user?.team?.filter((isTeam) => isTeam?.leader !== team?.leader);
+
+        jwt_axios.patch(`/team/${user?.team[0].leader}`, { members: [...restMembers] }).then((res) => {
+            if (res.data.acknowledged) {
+                jwt_axios
+                    .patch(`/user/${email}`, { team: [...restTeam] })
+                    .then((res) => {
+                        teamRefetch();
+                        toast.success("Member Removed");
+                    })
+                    .catch((error) => console.log(error));
+            }
+        });
+    };
+
+    if (userLoading || teamLoading) {
+        return (
+            <DashBoard title="Add Member" className="grid grid-cols-[repeat(auto-fill,_minmax(20.5rem,_1fr))] gap-6">
+                {[...Array(6)].map((user, index) => (
+                    <MemberCardLoader key={index} />
                 ))}
-            </div>
-        </DashBoard>
-    );
+            </DashBoard>
+        );
+    } else {
+        return (
+            <DashBoard title="Add Member">
+                {team?.leader === user?.email ? <AddTeamMember handleAddMember={handleAddMember} /> : ""}
+                <div className="mt-6 grid grid-cols-[repeat(auto-fill,_minmax(20.5rem,_1fr))] gap-6">
+                    {team?.members?.map((member, index) => (
+                        <Member key={index} member={member} handleDeleteCard={handleDeleteCard} />
+                    ))}
+                </div>
+            </DashBoard>
+        );
+    }
 };
 
 export default AddMember;
