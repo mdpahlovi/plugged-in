8742f1cb154acdc0dd1f6f10c94cfabd8692a122
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-02-10 15:20:18 +0600

    Peoples Route Added

---
+++ pages/dashboard/peoples.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Dashboard from "../../components/Layout/Dashboard";
+
+const Peoples = () => {
+    return <Dashboard title="Peoples of PluggedIn">This is peoples Route</Dashboard>;
+};
+
+export default Peoples;
---
+++ utilities/readFile.js
@@ -0,0 +1,10 @@
+export const readFileAsBase64 = async (file) => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+        reader.onerror = reject;
+        reader.readAsDataURL(file);
+    });
+};
