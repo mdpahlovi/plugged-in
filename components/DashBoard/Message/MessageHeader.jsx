b24888a8bcb746e3cbd8a95c43f7acadacbc22e3
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-02-14 14:24:25 +0600

    Message Error Fixed

--- components/DashBoard/Message/MessageHeader.jsx
+++ components/DashBoard/Message/MessageHeader.jsx
@@ -3,46 +3,33 @@ import NoPhoto from "../../../public/images/no-photo.jpg";
 import { useQuery } from "@tanstack/react-query";
 
 const MessageHeader = ({ userLoading, user, roomName }) => {
-  const { data: room } = useQuery({
-    queryKey: ["singleRoom", roomName],
-    queryFn: () =>
-      fetch(
-        `https://plugged-in-server.onrender.com/singleRoom?roomName=${roomName}`
-      ).then((res) => res.json()),
-  });
+    const { data: room } = useQuery({
+        queryKey: ["singleRoom", roomName],
+        queryFn: () => fetch(`https://plugged-in-server.onrender.com/singleRoom?roomName=${roomName}`).then((res) => res.json()),
+    });
 
-  if (userLoading) {
-    return (
-      <div className="relative flex items-center px-4 py-3 border-b animate-pulse">
-        <div className="w-10 h-10 mask mask-squircle bg-base-content/10" />
-        <div className="ml-3 h-7 w-28 rounded bg-base-content/10" />
-      </div>
-    );
-  } else {
-    return (
-      <div className="relative flex items-center px-4 py-3 border-b">
-        <Image
-          className="mask mask-squircle"
-          src={
-            room?.roomType === "single"
-              ? user?.avatar
-                ? user.avatar
-                : NoPhoto
-              : room?.avatar
-              ? room.avatar
-              : NoPhoto
-          }
-          alt="user"
-          width={40}
-          height={40}
-        />
-        <h3 className="ml-3 text-xl font-bold">
-          {room?.roomType === "single" ? user?.name : room?.teamName}
-        </h3>
-        <ActiveStatus />
-      </div>
-    );
-  }
+    if (userLoading) {
+        return (
+            <div className="relative flex items-center px-4 py-3 border-b animate-pulse">
+                <div className="w-10 h-10 mask mask-squircle bg-base-content/10" />
+                <div className="ml-3 h-7 w-28 rounded bg-base-content/10" />
+            </div>
+        );
+    } else {
+        return (
+            <div className="relative flex items-center px-4 py-3 border-b">
+                <Image
+                    className="mask mask-squircle"
+                    src={room?.roomType === "single" ? (user?.avatar ? user.avatar : NoPhoto) : room?.avatar ? room.avatar : NoPhoto}
+                    alt="user"
+                    width={40}
+                    height={40}
+                />
+                <h3 className="ml-3 text-xl font-bold">{room?.roomType === "single" ? (user?.name ? user.name : "No Name") : room?.teamName}</h3>
+                <span className="absolute w-3 h-3 bg-green-600 rounded-full left-10 top-2.5"></span>
+            </div>
+        );
+    }
 };
 
 export default MessageHeader;
--- pages/dashboard/message/[roomName].js
+++ pages/dashboard/message/[roomName].js
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from "react";
-import Image from "next/image";
 import Message from "../../../components/Layout/Message";
 import MessageHeader from "../../../components/DashBoard/Message/MessageHeader";
 import { useRouter } from "next/router";
@@ -8,48 +7,33 @@ import useGetUser from "../../../hooks/useGetUser";
 import { useQuery } from "@tanstack/react-query";
 import ScrollToBottom from "react-scroll-to-bottom";
 import { SocketContext } from "../../../contexts/SocketProvider";
-// import NoPhoto from "../../../public/images/no-photo.jpg";
 import { jwt_axios } from "../../../utilities/api";
 import EmojiPicker from "emoji-picker-react";
-
-import UserChatBubble from "../../../components/Layout/UserChatBubble";
-import OtherChatBubble from "../../../components/Layout/OtherChatBubble";
+import UserChatBubble from "../../../components/DashBoard/Message/UserChatBubble";
+import OtherChatBubble from "../../../components/DashBoard/Message/OtherChatBubble";
+import { GrEmoji } from "react-icons/gr";
+import { ImAttachment } from "react-icons/im";
+import { MdSend } from "react-icons/md";
 
 const ChatSection = () => {
-  const { query } = useRouter();
-  const { authUser } = useAuth();
-  const [oppositeUserEmail, setOppositeUserEmail] = useState("");
-  const { userLoading, user } = useGetUser(oppositeUserEmail);
-  const { data: room, refetch: roomsRefetch } = useQuery({
-    queryKey: ["singleRoom", query],
-    queryFn: () =>
-      fetch(
-        `https://plugged-in-server.onrender.com/singleRoom?roomName=${query?.roomName}`
-      ).then((res) => res.json()),
-  });
+    const { query } = useRouter();
+    const { authUser } = useAuth();
+    const [oppositeUserEmail, setOppositeUserEmail] = useState("");
+    const { userLoading, user } = useGetUser(oppositeUserEmail);
+    const { data: room, refetch: roomsRefetch } = useQuery({
+        queryKey: ["singleRoom", query],
+        queryFn: () => fetch(`https://plugged-in-server.onrender.com/singleRoom?roomName=${query?.roomName}`).then((res) => res.json()),
+    });
 
-  useEffect(() => {
-    if (query?.roomName?.includes("@")) {
-      if (authUser?.email === query?.roomName?.split("_")[0]) {
-        setOppositeUserEmail(query?.roomName?.split("_")[1]);
-      } else {
-        setOppositeUserEmail(query?.roomName?.split("_")[0]);
-      }
-    }
-  }, [authUser, query]);
-  const { socket } = useContext(SocketContext);
-  const [msgContent, setMsgContent] = useState("");
-  const [receivedMsg, setReceivedMsg] = useState(null);
-  const [typing, setTyping] = useState(null);
-
-  const { data: messages = [], refetch: messagesRefetch } = useQuery({
-    queryKey: ["getMessages", query],
-    queryFn: () =>
-      fetch(
-        `https://plugged-in-server.onrender.com/getMessages?roomName=${query?.roomName}`
-      ).then((res) => res.json()),
-  });
-
+    useEffect(() => {
+        if (query?.roomName?.includes("@")) {
+            if (authUser?.email === query?.roomName?.split("_")[0]) {
+                setOppositeUserEmail(query?.roomName?.split("_")[1]);
+            } else {
+                setOppositeUserEmail(query?.roomName?.split("_")[0]);
+            }
+        }
+    }, [authUser, query]);
     const { socket } = useContext(SocketContext);
     const [msgContent, setMsgContent] = useState("");
     const [receivedMsg, setReceivedMsg] = useState(null);
@@ -59,8 +43,6 @@ const ChatSection = () => {
         queryKey: ["getMessages", query],
         queryFn: () => fetch(`https://plugged-in-server.onrender.com/getMessages?roomName=${query?.roomName}`).then((res) => res.json()),
     });
-    console.log(messages);
-    console.log(query?.roomName);
 
     const handleSendMsg = () => {
         if (msgContent) {
@@ -102,47 +84,64 @@ const ChatSection = () => {
         }
     }, [receivedMsg]);
 
+    const sendMsgToDb = (sendingMsg) => {
+        jwt_axios.put("https://plugged-in-server.onrender.com/messageStore", sendingMsg).then((res) => {
+            if (res?.data?.acknowledged) {
+                messagesRefetch();
+                roomsRefetch();
+            }
+        });
+    };
 
-  return (
-    <Message>
-      <MessageHeader
-        user={user}
-        roomName={query?.roomName}
-        userLoading={userLoading}
-      />
-      <ScrollToBottom className="h-[70%]">
-        <div className="px-4 py-3">
-          {messages?.map((message, index) =>
-            message?.authorEmail === authUser?.email ? (
-              <UserChatBubble kay={index} message={message} />
-            ) : (
-              <OtherChatBubble kay={index} message={message} />
-            )
-          )}
-          {typing && typing.roomName == query?.roomName && (
-            <p className="bg-black px-5 py-2 rounded-xl text-white inline font-bold absolute bottom-0 mb-2">
-              Typing...
-            </p>
-          )}
-        </div>
-      </ScrollToBottom>
-      {/* <MessageFooter /> */}
-      <div className="h-max flex items-center justify-between px-4 py-3 border-t gap-3">
-        <div className="dropdown dropdown-top">
-          <label tabIndex={0}>
-            <Icons className="w-8 h-8 cursor-pointer" />
-          </label>
-          <div
-            tabIndex={0}
-            className="dropdown-content p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <div>
-              <EmojiPicker
-                onEmojiClick={(event, emojiObject) => {
-                  console.log("emoji clicked", event.emoji);
-                  setMsgContent(msgContent + event.emoji);
-                }}
-              />
+    return (
+        <Message>
+            <MessageHeader user={user} roomName={query?.roomName} userLoading={userLoading} />
+            <ScrollToBottom className="overflow-y-auto">
+                <div className="px-4 py-3">
+                    {messages?.map((message, index) =>
+                        message?.authorEmail === authUser?.email ? (
+                            <UserChatBubble key={index} message={message} />
+                        ) : (
+                            <OtherChatBubble key={index} message={message} />
+                        )
+                    )}
+                    {typing && typing.roomName == query?.roomName && (
+                        <p className="bg-black px-5 py-2 rounded-xl text-white inline font-bold absolute bottom-0 mb-2">Typing...</p>
+                    )}
+                </div>
+            </ScrollToBottom>
+            {/* <MessageFooter /> */}
+            <div className="h-max flex items-center justify-between px-4 py-3 border-t gap-3">
+                <div className="dropdown dropdown-top">
+                    <label tabIndex={0}>
+                        <GrEmoji className="text-3xl cursor-pointer" />
+                    </label>
+                    <div tabIndex={0} className="dropdown-content mb-5">
+                        <EmojiPicker
+                            onEmojiClick={(event, emojiObject) => {
+                                console.log("emoji clicked", event.emoji);
+                                setMsgContent(msgContent + event.emoji);
+                            }}
+                        />
+                    </div>
+                </div>
+                <ImAttachment className="text-3xl cursor-pointer" />
+                <input
+                    type="text"
+                    placeholder="Message"
+                    className="input rounded-full"
+                    value={msgContent}
+                    name="message"
+                    onChange={(event) => {
+                        setMsgContent(event.target.value);
+                        socket.emit("typing", {
+                            msgContent,
+                            roomName: query?.roomName,
+                        });
+                    }}
+                    required
+                />
+                <MdSend onClick={handleSendMsg} className="text-3xl cursor-pointer" />
             </div>
         </Message>
     );
